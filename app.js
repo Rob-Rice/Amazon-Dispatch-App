8381e2ec17a459d8213de57ccd02f33dffad937b
Rob-Rice <robrice93@yahoo.com>
2021-12-26 07:50:05 -0600

    javascripted

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-// ----------------- selectors ----------------- 
+// ----------------- setters ----------------- 
 let date = document.querySelector('.date');
-
-
-// ----------------- date at top of page ----------------- 
 let today = new Date();
 let day = `${today.getDate() < 10 ? '0' : ""}${today.getDate()}`;
 let month = `${(today.getMonth() + 1) < 10 ? '0' : ""}${today.getMonth() + 1}`;
 let year = today.getFullYear();
+let count = 0;
 
+// ----------------- selectors ----------------- 
+const value = document.getElementById('value');
+const btns = document.querySelectorAll('.btn');
+
+
+
+// ----------------- date at top of page ----------------- 
 date.textContent = `${day}/${month}/${year}`;
+
+// ----------------- functions ----------------- 
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        } else if (styles.contains('increase')) {
+            count++;
+        }
+
+
+        value.textContent = count;
+    });
+});
