ac993203b792a9a9c5817aa4c1603f5f15bb2a3b
Rob-Rice <robrice93@yahoo.com>
2021-12-30 22:24:25 -0600

    new commit
    changed JavaScript & add some new buttons

--- app.js
+++ app.js
@@ -9,11 +9,8 @@ let count = 0;
 // ----------------- selectors ----------------- 
 const value = document.getElementById('value');
 const btns = document.querySelectorAll('.btn');
-const driverInput = document.querySelector('.driver-input');
-const cxInput = document.querySelector('.route-number');
-const vanInput = document.querySelector('.driver-van');
 const addBtn = document.querySelector('.add-btn');
-const driverList = document.querySelector('.driver-list');
+
 
 
 
@@ -23,53 +20,123 @@ date.textContent = `${day}/${month}/${year}`;
 // ----------------- functions ----------------- 
 
 // van number counter
-btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        e.preventDefault(); // stop form from submitting
-        const styles = e.currentTarget.classList;
-        if(styles.contains('decrease')) { // adding to count
-            count--;
-        } else if (styles.contains('increase')) { // subtracting from count 
-            count++;
+// btns.forEach(btn => {
+//     btn.addEventListener('click', (e) => {
+//         e.preventDefault(); // stop form from submitting
+//         const styles = e.currentTarget.classList;
+//         if(styles.contains('delete')) { // subtracting to count
+//             count--;
+//         } else if (styles.contains('increase')) { // add from count 
+//             count++;
+//         } 
+
+//         value.textContent = count;
+//     });
+// });
+
+
+class Driver {
+    constructor(driver, cx, van) {
+      this.driver = driver;
+      this.cx = cx;
+      this.van = van;
+    }
+    }
+
+    // UI Class: Handle UI Tasks
+    class UI {
+    static displayDrivers() {
+        const drivers = [
+                    {
+                        driver: 'Roberto Rice',
+                        cx: '144',
+                        van: '33'
+                    },
+                    {
+                        driver: 'Rob Rice',
+                        cx: '14',
+                        van: 'R 33'
+                    }
+                ];
+
+        drivers.forEach((name) => UI.addDriverToList(name));
+    }
+
+    static addDriverToList(name) {
+        const list = document.querySelector('#driver-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td><a href="#" class="btn delete">X</a></td>
+        <td class="drivers-name">${name.driver}</td>
+        <td class="drivers-cx">${name.cx}</td>
+        <td class="drivers-van">${name.van}</td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static deleteDriver(el) {
+        if(el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
         }
+    } 
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className= `alert-message`;
+        div.appendChild(document.createTextNode(message));
+        const body = document.querySelector('.body');
+        const form = document.querySelector('#driver-form');
+        body.insertBefore(div, form);
+    }
 
-        value.textContent = count;
-    });
+    static clearFields() {
+        document.querySelector('#driver-input').value = '';
+        document.querySelector('#cx-input').value = '';
+        document.querySelector('#van-input').value = '';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', UI.displayDrivers);
+
+// add driver
+document.querySelector('#driver-form').addEventListener('submit', (event) => {
+    // prevent form from submitting
+    event.preventDefault();
+    // get form values 
+    const driverName = document.querySelector('#driver-input').value;
+    const cx = document.querySelector('#cx-input').value;
+    const van = document.querySelector('#van-input').value;
+
+    // validate
+    if(driverName === '' || cx === '' || van === '') {
+        // alert('Please fill in all fields');
+        UI.showAlert('Please fill in all fields');
+    } else {
+        // instatiate book
+        const driver = new Driver(driverName, cx, van);
+
+        // add add to ui
+        UI.addDriverToList(driver);
+
+        // clear fields 
+        UI.clearFields();
+    }
+
+    // // instatiate book
+    // const driver = new Driver(driverName, cx, van);
+
+    // // add add to ui
+    // UI.addDriverToList(driver);
+
+    // // clear fields 
+    // UI.clearFields();
 });
 
-// add drivers to list
-addBtn.addEventListener('click', addDriver);
 
-function addDriver(event) {
-    // driver div
-    const driverDiv = document.createElement('div'); 
-    driverDiv.classList.add('driverAdded');
-    // create the li for driver
-    const newDriver = document.createElement('li');
-    newDriver.innerText = driverInput.value;
-    newDriver.classList.add('driver-item');
-    driverDiv.appendChild(newDriver);
-    // create the li for cx #
-    const newCx = document.createElement('li');
-    newCx.innerText = cxInput.value;
-    newCx.classList.add('cx-item');
-    driverDiv.appendChild(newCx);
-    // create the li for van #
-    const newVan = document.createElement('li');
-    newVan.innerText = vanInput.value;
-    newVan.classList.add('cx-item');
-    driverDiv.appendChild(newVan);
-    // delete button
-    const deleteDriver = document.createElement('button');
-    deleteDriver.innerHTML = '<i class="fas fa-times-circle"></i>';
-    deleteDriver.classList.add('delete')
-    driverDiv.appendChild(deleteDriver);
-    // append to list
-    driverList.appendChild(driverDiv);
-    // clear input area
-    driverInput.value = '';
-    cxInput.value = '';
-    vanInput.value = '';
-
-}
+// remove driver
+document.querySelector('#driver-list').addEventListener('click', (e) => {
+    UI.deleteDriver(e.target);
+});
